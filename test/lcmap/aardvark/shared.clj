4819eb80826062d996fc48a3ad99eb2a973819e7
David Hill <davidvhill@users.noreply.github.com>
2016-11-16 20:24:32 -0600

    switched config format to edn, corrected config failures for langhor (#3)

--- test/lcmap/aardvark/shared.clj
+++ test/lcmap/aardvark/shared.clj
@@ -8,22 +8,22 @@
 (defn test-system
   "Start the app."
   ([]
-   (test-system "lcmap-landsat-test.ini"))
+   (test-system "lcmap-landsat-test.edn"))
   ([cfg-path]
    (try
      (log/debugf "Starting app with `%s`" cfg-path)
-     (log/debugf "Configuration: %s" (config/build {:ini cfg-path}))
-     (-> {:ini cfg-path}
+     (log/debugf "Configuration: %s" (config/build {:edn cfg-path}))
+     (-> {:edn cfg-path}
          config/build
          system/system)
      (catch java.io.FileNotFoundException e
-       (log/error "File not found, is `lcmap-landsat-test.ini` on the load path?")))))
+       (log/error "File not found, is `lcmap-landsat-test.edn` on the load path?")))))
 
 (defmacro with-system
   "Start and stop the system, useful for integration tests."
   [[binding] & body]
   `(let [~binding (component/start (test-system
-                                     (io/resource "lcmap-landsat-test.ini")))]
+                                     (io/resource "lcmap-landsat-test.edn")))]
      (try
        (do ~@body)
        (finally
