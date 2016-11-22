c9b0c04e8ffcfcdd21e8eaebe16542d6a977ca71
davidvhill <davehill75@gmail.com>
2016-11-22 12:21:58 -0600

    removing unused code

--- test/lcmap/aardvark/shared.clj
+++ test/lcmap/aardvark/shared.clj
@@ -4,23 +4,12 @@
             [mount.core :as mount]
             [clojure.java.io :as io]))
 
-(defn test-config
-  "Start the app."
-  ([]
-   (test-system "lcmap-landsat-test.edn"))
-  ([cfg-path]
-   (try
-     (log/debugf "Starting app with `%s`" cfg-path)
-     (let [cfg (config/build {:edn cfg-path})]
-       (mount/start-with {#'lcmap.aardvark.state/config cfg}))
-     (catch java.io.FileNotFoundException e
-       (log/error "File not found, is `lcmap-landsat-test.edn` on the load path?")))))
-
 (defmacro with-system
   "Start and stop the system, useful for integration tests."
   [[binding] & body]
   `(let [cfg (config/build {:edn (io/resource "lcmap-landsat-test.edn")})
          ~binding (mount/start-with {#'lcmap.aardvark.state/config cfg})]
+     (log/debug "Starting test system with config:" cfg)
      (try
        (do ~@body)
        (finally
