288fcc8d4584cb5f3500b50815fb554187eae0ef
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-12-21 09:21:25 -0600

    Tidy-up dev fixtures

--- dev/lcmap/aardvark/dev.clj
+++ dev/lcmap/aardvark/dev.clj
@@ -2,19 +2,21 @@
   (:require
             [clojure.tools.namespace.repl :refer [refresh refresh-all]]
             [clojure.java.io :as io]
+            [clojure.edn :as edn]
             [mount.core :as mount]
             [lcmap.aardvark.state :as state]
             [lcmap.aardvark.config :as config]
             [lcmap.aardvark.server :as server]
             [lcmap.aardvark.worker :as worker]
             [uberconf.core :as uberconf]
-            [lcmap.aardvark.fixtures :refer [load-tile-spec]]))
+            [lcmap.aardvark.fixtures :as fixtures]))
 
 (defn start
   "Start dev system with a replacement config namespace"
   []
-  (let [cfg (config/build {:edn (io/resource "lcmap-landsat.edn")})]
-    (mount/start-with {#'lcmap.aardvark.config/config cfg})))
+  (let [cfg (edn/read-string (slurp (io/resource "lcmap-landsat.edn")))]
+    (-> (mount/with-args {:config cfg})
+        (mount/start))))
 
 (defn stop
   "Stop system"
