5c95539dd268c8ec516c2491046612ae8112bfb9
davidvhill <davehill75@gmail.com>
2017-01-22 16:33:23 -0600

    replaced (log/debug) calls with (log/spy)

--- src/lcmap/aardvark/tile_spec_index.clj
+++ src/lcmap/aardvark/tile_spec_index.clj
@@ -14,7 +14,6 @@
   [tile-spec]
   (log/debugf "appending additional tags to tile-spec")
   (let [ubid-tags (str/split (:ubid tile-spec) #"/|_")]
-    (log/tracef "====> for tile-spec: %s" tile-spec)
     (if (> (count ubid-tags) 1)
       (update tile-spec :tags conj ubid-tags)
       tile-spec)))
@@ -23,7 +22,6 @@
    "Converts a tile-spec to an index entry."
   [tile-spec]
   (log/debugf "converting tile-spec to index entry")
-  (log/tracef "====> for tile-spec: %s" tile-spec)
   (str (json/write-str {"index" {"_retry_on_conflict" 3}}) "\n"
        (json/write-str tile-spec) "\n"))
 
@@ -31,7 +29,7 @@
   "Clears the tile-spec index"
   []
   (log/infof "clearing the tile-spec index")
-  (es/clear! (get-in config [:search :index-url])))
+  (log/spy :trace (es/clear! (get-in config [:search :index-url]))))
 
 (defn search
   "Submits a query to the index.  Query should conform to elasticsearch
@@ -41,22 +39,20 @@
 
   ([api-url query]
    (log/debugf "querying the index")
-   (log/debugf "====> api-url: %s query: %s" api-url query)
    (es/search api-url query (get-in config [:search :max-result-size]))))
 
 (defn result
   "Extracts tile-specs from search results"
   [search-results]
   (log/debugf "extracting tile-specs from search results")
-  (log/tracef "====> for search-results: %s" search-results)
-  (map #(get % "_source")
-       (get-in search-results ["hits" "hits"])))
+  (log/spy :trace
+           (map #(get % "_source")(get-in search-results ["hits" "hits"]))))
 
 (defn save
   "Saves tile-specs to the ElasticSearch index."
   [tile-specs]
   (log/infof "saving index entries")
-  (log/tracef "====> for tile-spec: %s" tile-specs)
-  (es/load! (get-in config [:search :bulk-api-url])
-            (pmap #(->> % +tags index-entry) (vectorize tile-specs)))
+  (log/spy :trace
+           (es/load! (get-in config [:search :bulk-api-url])
+                     (pmap #(->> % +tags index-entry) (vectorize tile-specs))))
   tile-specs)
