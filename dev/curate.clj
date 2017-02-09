04369824d7ee05e7d8d323261341923e1a55f181
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-02-09 13:53:56 -0600

    Add ns for curating tile-specs and sources

---
+++ dev/curate.clj
@@ -0,0 +1,33 @@
+(ns curate
+  "Provides data curator with tools to load data and check progress."
+  (:require [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [lcmap.aardvark.source :as source]
+            [lcmap.aardvark.tile-spec :as tile-spec]))
+
+(defn read-edn
+  "Read EDN at path. Useful for loading tile-specs and sources."
+  [path]
+  (-> path io/file slurp edn/read-string))
+
+(defn save-tile-specs
+  "Save all tile-specs in EDN at path."
+  [path]
+  (->> path read-edn (map tile-spec/save)))
+
+(defn save-sources
+  "Save all sources in EDN at path."
+  [path n]
+  (->> path read-edn (take n) (map source/save)))
+
+(comment
+  (save-tile-specs "data/tile-specs/L4.edn")
+  (save-tile-specs "data/tile-specs/L5.edn")
+  (save-tile-specs "data/tile-specs/L7.edn")
+  (save-tile-specs "data/tile-specs/L8.edn"))
+
+(comment
+  (save-sources "data/sources/washington/LT04046026.edn" 1)
+  (save-sources "data/sources/washington/LT05046026.edn" 1)
+  (save-sources "data/sources/washington/LE07046026.edn" 1)
+  (save-sources "data/sources/washington/LC8046026.edn"  1))
