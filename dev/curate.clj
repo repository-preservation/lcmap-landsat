2d42ba2060bbaa663f0fa89a97aeb662c7feecd0
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2017-02-24 22:39:22 -0600

    Update README and curation docs.
    - Ignore a recommended curation related configuration file that will probably contain sensitive credentials.
    - Organize curation and add a lot more detailed about its usage.
    - Make usage examples more consistent in the README.
    - Remove dev/resources/schema.teardown.cql because it is optional.
    - Update `user` ns docs, remove unused requires, explain it's usage and when it should and shouldn't be used.

--- dev/curate.clj
+++ dev/curate.clj
@@ -1,6 +1,29 @@
 (ns curate
-  "Provides data curator with tools to load data and check progress."
+  "Provides data curators with data management tools.
+
+  WARNING: PLEASE READ THIS ENTIRELY BEFORE USING THESE FUNCTIONS.
+
+  These function can be used with local and operational environments;
+  but exercise caution before using this with a production system.
+
+  Make sure you have up-to-date code and submodules. If you don't know
+  exactly how to do this, talk with someone that does! Loading stale
+  data in a production system could cause subtle problems.
+
+  This namespace contains a `start` function that will limit the mount
+  beings to those needed to load data. It does not start a server or
+  worker, and it does not execute DB schema setup or teardown logic.
+
+  However, you are *strongly* advised to use credentials with minimal
+  permissions required to curate data (selecting and inserting data)
+  so that you you don't have to worry about performing schema changes.
+
+  You will need to create and update a copy of `dev/lcmap.landsat.edn`
+  named `curator.edn` and place it on the resource load path. This file
+  is listed in .gitignore so you don't have to worry about sensitive
+  data being added to the repository."
   (:require [lcmap.aardvark.config :as config]
+            [lcmap.aardvark.db :as db]
             [lcmap.aardvark.event :as event]
             [lcmap.aardvark.server :as server]
             [lcmap.aardvark.source :as source]
@@ -10,10 +33,48 @@
             [lcmap.aardvark.worker :as worker]
             [mount.core :as mount]))
 
+
+;; STEP 1. Start the system and make sure it references the
+;; services you expect. If you didn't create `curator.edn`
+;; then you didn't do the required reading. Go back to
+;; the beginning of this file and read the documentation!
+
+(defn start
+  "Start enough of the system to invoke ingest."
+  []
+  (->> (mount/with-args {:config (util/read-edn "curator.edn")})
+       (mount/start-without #'lcmap.aardvark.server/server
+                            #'lcmap.aardvark.worker/worker
+                            #'lcmap.aardvark.worker/worker-consumer
+                            #'lcmap.aardvark.db/db-schema)))
+
+;; Verify that you are referencing the correct backing services.
+
+(comment
+  (start)
+  (-> config/config :database :cluster :contact-points)
+  (-> config/config :event :host)
+  (-> config/config :search :index-url))
+
+;; STEP 2. Create tile-specs. Without tile-specs, the application
+;; won't know where to store tile data. Please confirm you have
+;; the most recent `data` submodule before using this.
+
 (defn save-tile-specs
   "Save all tile-specs in EDN at path."
   [path]
   (->> path util/read-edn (map tile-spec/save)))
+
+(comment
+  (save-tile-specs "tile-specs/L4.edn")
+  (save-tile-specs "tile-specs/L5.edn")
+  (save-tile-specs "tile-specs/L7.edn")
+  (save-tile-specs "tile-specs/L8.edn"))
+
+;; STEP 3. Create some sources. This will create data in Cassandra
+;; and will send messages to an exchange that are routed to a queue
+;; consumed by a worker. This does not prevent the same source from
+;; being enqueued for ingest multiple times.
 
 (defn save-sources
   "Save all sources in EDN at path."
@@ -22,6 +83,27 @@
   ([path]
    (->> path util/read-edn (map source/save))))
 
+;; Notice that this will enque the first 25 sources listed in
+;; each EDN file. For an operational system, it is safe to save
+;; tens of thousands of sources. To do this, map over all of
+;; the EDN files in a directory with `save-sources`.
+
+(comment
+  (save-sources "sources/washington/LT04046027.edn" 25)
+  (save-sources "sources/washington/LT05046027.edn" 25)
+  (save-sources "sources/washington/LE07046027.edn" 25)
+  (save-sources "sources/washington/LC8046027.edn"  25))
+
+;; Step 4: See how things are going. This will inspect the
+;; sources table for activity and check the most recent
+;; activity. It is advisable to use Kibana (centralized logging
+;; in an operational environment) to get detailed information
+;; about ingest activity.
+;;
+;; PLEASE NOTE:
+;; IF NO WORKERS ARE RUNNING THEN THERE WILL BE NO PROGRESS.
+;;
+
 (defn progress-report
   "Group sources together by most recent progress/state with count."
   [path]
@@ -29,46 +111,7 @@
        (map (comp :progress_name last source/search :id))
        (frequencies)))
 
-(defn start
-  "Start enough of the system to invoke ingest.
-
-  Does not start the HTTP server or AMQP worker; see `dev/user.clj`
-  if you want to start the full system with a REPL."
-  []
-  (->> (mount/with-args {:config (util/read-edn "lcmap.aardvark.edn")})
-       (mount/start-without #'lcmap.aardvark.server/server
-                            #'lcmap.aardvark.worker/worker
-                            #'lcmap.aardvark.worker/worker-consumer
-                            #'lcmap.aardvark.db/db-schema)))
-
 (comment
-  "Step 0: Configuration!
-   Update `dev/resources/lcmap.aardvark.edn` to use desired Cassandra,
-   Elasticsearch, and RabbitMQ instance. This is almost definitely *not*
-   localhost if you are curating an operational system.
-  "
-  (-> config/config :database :cluster :contact-points)
-  (-> config/config :event :host)
-  (-> config/config :search :index-url)
-
-  "Step 1: Start system."
-  (start)
-
-  "Step 2: Create tile-specs, otherwise no tiles will be saved."
-  (save-tile-specs "tile-specs/L4.edn")
-  (save-tile-specs "tile-specs/L5.edn")
-  (save-tile-specs "tile-specs/L7.edn")
-  (save-tile-specs "tile-specs/L8.edn")
-
-  "Step 3: Enqueue one path-row from each mission. If the scene
-  has already been processed, it will be processed again."
-  (save-sources "sources/washington/LT04046026.edn" 1)
-  (save-sources "sources/washington/LT05046026.edn" 1)
-  (save-sources "sources/washington/LE07046026.edn" 1)
-  (save-sources "sources/washington/LC8046026.edn"  1)
-
-  "Step 5: See how things are going. This will get the most recent
-   progress name for each source and group them together by count."
   (progress-report "sources/washington/LT04046026.edn")
   (progress-report "sources/washington/LT05046026.edn")
   (progress-report "sources/washington/LE07046026.edn")
--- dev/user.clj
+++ dev/user.clj
@@ -1,38 +1,39 @@
 (ns user
-  "This is the entry-point for REPL sessions, useful for development.
+  "This is the entry-point for REPL sessions.
 
-   Run `init` to create the Cassandra schema and RabbitMQ exchanges,
-   queues, and bindings.
+  Functions in this namespace are useful for setting up and running
+  the project in a local development environment. Exercise caution
+  when using a REPL that is connected to an operational environment.
 
-   Please see the following files in dev/resources:
-   - lcmap.aardvark.edn: connection information to backing services
-   - event.setup.edn: essentially a 'schema' for RabbitMQ
-   - schema.setup.cql: create Cassandra keyspaces, tables, indexes
-   - schema.teardown.cql: remove Cassandra keyspaces, etc..."
+  Should you need to override default configuration, please see the
+  following files in dev/resources:
 
-  ;; Requirement order of lcmap namespaces are important because they
-  ;; may affect the order in which states are started and stopped.
-  (:require [clojure.java.io :as io]
-            [clojure.tools.namespace.repl :refer [refresh refresh-all]]
-            [lcmap.aardvark.config :as config]
-            [lcmap.aardvark.db :as db]
-            [lcmap.aardvark.event :as event]
+  - lcmap.aardvark.edn: contains connection information to backing
+    services (Cassandra, RabbitMQ, and Elasticsearch).
+
+  - event.setup.edn: defines the queues, exchanges, and bindings
+    for RabbitMQ.
+
+  - schema.setup.cql: CQL that will create Cassandra keyspaces, tables,
+    and indexes."
+  (:require [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+            [lcmap.aardvark.server :as server]
+            [lcmap.aardvark.worker :as worker]
             [lcmap.aardvark.util :as util]
             [mount.core :as mount]))
 
 (defn start
   "Start stateful 'beings' using an EDN-derived configuration.
 
-   If the system has not been initialized the db/db-session will
-   fail to start because the default keyspace cannot be found,
-   this can be solved by running `init`"
+  Any namespaces that have been required that define mount 'beings' will
+  be started. If you evaluate other namespaces during development, they
+  will also be started when this function is invoked."
   []
-  (-> {:config (util/read-edn "lcmap.aardvark.edn")}
-      (mount/with-args)
+  (-> (mount/with-args {:config (util/read-edn "lcmap.aardvark.edn")})
       (mount/start)))
 
 (defn stop
-  "Stop system."
+  "Stop stateful 'beings'"
   []
   (mount/stop))
 
